37ed9c225fabeeeb9a228134ed7a0e604c048fe6
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-10-04 11:12:47 -0700

    created userRoutes

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,19 @@
+const router = require('express').Router();
+const {
+    getSingleUser,
+    getUsers,
+    createUser,
+    updateUser,
+    deleteUser,
+} = require('../../controllers/userController');
+
+router.route('/').get(getUsers).post(createUser);
+
+router
+    .route('/:userId')
+    .get(getSingleUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+
+module.exports = router;
