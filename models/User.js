33bee4875039076d2c36c5e93c6a35b8d52f0a2a
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-10-04 11:03:54 -0700

    updated user model

--- models/User.js
+++ models/User.js
@@ -1,23 +1,41 @@
 const { Schema, model } = require('mongoose');
 
+const validateEmail = (email) => {
+    const re = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    return re.test(email);
+};
+
 // Schema to create User model
 const userSchema = new Schema(
     {
-        username: String,
-        email: String,
-        age: Number,
-        thoughts: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'Thought',
-            },
-        ],
-        friends: [
-            {
-                type: Schema.Types.ObjectId,
-                ref: 'User',
-            },
-        ],
+        username: {
+            type: String,
+            required: true.valueOf,
+            unique: true,
+            trim: true
+        },
+        email: {
+            type: String,
+            required: true,
+            unique: true,
+            validate: [validateEmail, "Please fill a valid email address"],
+            match: [
+                /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/,
+                "Please fill a valid email address",
+            ],
+        },
+        // thoughts: [
+        //     {
+        //         type: Schema.Types.ObjectId,
+        //         ref: 'Thought',
+        //     },
+        // ],
+        // friends: [
+        //     {
+        //         type: Schema.Types.ObjectId,
+        //         ref: 'User',
+        //     },
+        // ],
     },
     {
         toJSON: {
