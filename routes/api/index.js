032b999c1f51ed9e24ebebe495921fb6f1d44f06
Nabhahn Kamaal <NabhahnK@gmail.com>
2022-10-04 13:24:01 -0700

    updated index.js in api folder

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,8 +1,8 @@
 const router = require('express').Router();
 const userRoutes = require('./userRoutes');
-// const tagRoutes = require('./tagRoutes');
+const thoughtRoutes = require('./thoughtRoutes');
 
 router.use('/users', userRoutes);
-// router.use('/tags', tagRoutes);
+router.use('/thoughts', thoughtRoutes);
 
 module.exports = router;
